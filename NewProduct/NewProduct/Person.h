563967ad2d4f73c483d47342bcd62a741346228b
sunjinlong <sunjinlong@promote.cache-dns.local>
2017-12-07 10:52:36 +0800

    111

---
+++ NewProduct/NewProduct/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  NewProduct
+//
+//  Created by 孙金龙 on 2017/12/7.
+//  Copyright © 2017年 孙金龙. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ NewProduct/NewProduct/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  NewProduct
+//
+//  Created by 孙金龙 on 2017/12/7.
+//  Copyright © 2017年 孙金龙. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
